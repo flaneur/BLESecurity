a65f5919a63fe9733ec64db94bb50af1652afe5b
hfelek <huseyin.felek@eliarge.com>
2022-03-10 18:24:29 +0300

    Initial Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 /*
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
+HF
 */
 #include <Arduino.h>
 #include <BLEDevice.h>
